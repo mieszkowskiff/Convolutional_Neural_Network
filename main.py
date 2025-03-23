ca079cba109523d53ca2aa4c6b87509058a16378
mieszkowskiff <mieszkowskiff@gmail.com>
2025-03-23 01:08:54 +0100

    Refactor main.py to use a modular network architecture and improve model training

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 from torchvision import datasets, transforms
 import torch
 import time
+import tqdm
 
-class Block(torch.nn.Module):
+class InitBlock(torch.nn.Module):
+    def __init__(self, in_out_channels = 32):
+        super(InitBlock, self).__init__()
+        self.init_conv = torch.nn.Conv2d(3, in_out_channels, kernel_size = 3, stride = 1, padding = 1)
+    
+    def forward(self, x):
+        if self.training:
+            x = torch.nn.Sequential(
+                transforms.RandomAffine(degrees = 15, translate = (0.1, 0.1), scale = (0.8, 0.8)),
+                transforms.GaussianBlur(kernel_size = 3, sigma = (0.1, 2.0))
+            )
+        x = self.init_conv(x)
+        return torch.nn.functional.relu(x)
+
+
+
+class ConvolutionalBlock(torch.nn.Module):
     def __init__(self, in_out_channels = 32, internal_channels = 32): 
-        super(Block, self).__init__()
+        super(ConvolutionalBlock, self).__init__()
         self.convolutions = torch.nn.ModuleList([
             torch.nn.Sequential(
                 torch.nn.Conv2d(in_out_channels, internal_channels, kernel_size = 3, stride = 1, padding = 1),
@@ -16,14 +33,12 @@ class Block(torch.nn.Module):
     def forward(self, x, bypass = True):
         y = torch.cat([conv(x) for conv in self.convolutions], dim = 1)
         if bypass:
-            x = torch.nn.functional.relu(x + y)
-        else:
-            x = torch.nn.functional.relu(y)
-        return x
+            return torch.nn.functional.relu(y) + x
+        return torch.nn.functional.relu(y)
 
-class Head(torch.nn.Module):
+class HeadBlock(torch.nn.Module):
     def __init__(self, in_out_channels = 32):
-        super(Head, self).__init__()
+        super(HeadBlock, self).__init__()
         self.head = torch.nn.Sequential(
             torch.nn.Linear(in_out_channels * 32 * 32, 256),
             torch.nn.ReLU(),
@@ -40,19 +55,19 @@ class Head(torch.nn.Module):
 class Network(torch.nn.Module):
     def __init__(self, blocks_number, in_out_channels = 32, internal_channels = 32):
         super(Network, self).__init__()
-        self.init_conv = torch.nn.Conv2d(3, in_out_channels, kernel_size = 3, stride = 1, padding = 1)
+        self.init_block = InitBlock(in_out_channels = in_out_channels)
         self.blocks = torch.nn.Sequential(
             *[
-                Block(
+                ConvolutionalBlock(
                     in_out_channels = in_out_channels, 
                     internal_channels = internal_channels
                 ) for _ in range(blocks_number)
             ]
         )
-        self.head = Head(in_out_channels = in_out_channels)
+        self.head = HeadBlock(in_out_channels = in_out_channels)
 
     def forward(self, x):
-        x = self.init_conv(x)
+        x = self.init_block(x)
         x = torch.nn.functional.relu(x)
         x = self.blocks(x)
         x = self.head(x)
@@ -65,24 +80,15 @@ def main():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Using device: {device}")
 
-    train_transform = transforms.Compose([
-        transforms.ToTensor(),
-    ])
+    train_transform = transforms.ToTensor()
 
     test_transform = transforms.ToTensor()
-
-    train_dataset_usage = 0.1
 
     train_dataset = datasets.ImageFolder(root = "./data/train", transform = train_transform)
 
 
-    dataset_size = len(train_dataset)
-    subset_size = int(dataset_size * train_dataset_usage)
-    random_indices = torch.randperm(dataset_size)[:subset_size]
-    train_subset = torch.utils.data.Subset(train_dataset, random_indices)
-
     train_loader = torch.utils.data.DataLoader(
-        train_subset, 
+        train_dataset, 
         batch_size = 32, 
         shuffle = True, 
         pin_memory = True, 
@@ -111,7 +117,7 @@ def main():
         start_time = time.time()
         total_loss = 0
         model.train()
-        for images, labels in train_loader:
+        for images, labels in tqdm.tqdm(train_loader):
             images, labels = images.to(device), labels.long().to(device)
             optimizer.zero_grad()
             outputs = model(images)
@@ -122,7 +128,7 @@ def main():
         correctly_predicted = 0
         model.eval()
         with torch.no_grad():
-            for images, labels in test_loader:
+            for images, labels in tqdm.tqdm(test_loader):
                 images, labels = images.to(device), labels.long().to(device)
                 outputs = model(images)
                 preditctions = torch.argmax(outputs, dim = 1)
