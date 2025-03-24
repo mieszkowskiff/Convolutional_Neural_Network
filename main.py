4b4be7bf02553a55d1b8ec9170b10776667d757c
FokaWsrodMorskichFal <stas.kurzatkowski@gmail.com>
2025-03-24 01:22:32 +0100

    70 70 70 on fireeee

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from torchvision import datasets, transforms
 from torchsummary import summary
+from torch.amp import autocast, GradScaler
 import torch
 import time
 import tqdm
 
 class InitBlock(torch.nn.Module):
-    def __init__(self, in_out_channels = 32):
+    def __init__(self, out_channels = 32):
         super(InitBlock, self).__init__()
-        self.init_conv = torch.nn.Conv2d(3, in_out_channels, kernel_size = 3, stride = 1, padding = 1)
+        self.init_conv = torch.nn.Conv2d(3, out_channels, kernel_size = 3, stride = 1, padding = 1)
     
     def forward(self, x):
         if self.training:
@@ -17,33 +18,29 @@ class InitBlock(torch.nn.Module):
         x = self.init_conv(x)
         return torch.nn.functional.relu(x)
 
-
-
 class ConvolutionalBlock(torch.nn.Module):
-    def __init__(self, in_out_channels = 32, internal_channels = 32): 
+    def __init__(self, in_channels = 32, out_channels = 32, bypass = False): 
         super(ConvolutionalBlock, self).__init__()
-        self.convolutions = torch.nn.ModuleList([
-            torch.nn.Sequential(
-                torch.nn.Conv2d(in_out_channels, internal_channels, kernel_size = 3, stride = 1, padding = 1),
-                torch.nn.ReLU(),
-                torch.nn.Conv2d(internal_channels, in_out_channels, kernel_size = 3, stride = 1, padding = 1),
-            )
-        ])
+        self.bypass = bypass
+        self.convolution = torch.nn.Conv2d(in_channels, out_channels, kernel_size = 3, stride = 1, padding = 1)
     
-    def forward(self, x, bypass = False):
-        y = torch.cat([conv(x) for conv in self.convolutions], dim = 1)
-        if bypass:
+    def forward(self, x):
+        y = self.convolution(x)
+        if self.bypass:
             return torch.nn.functional.relu(y) + x
         return torch.nn.functional.relu(y)
 
 class HeadBlock(torch.nn.Module):
-    def __init__(self, in_out_channels = 32):
+    def __init__(self, in_channels = 32, size = 32):
         super(HeadBlock, self).__init__()
         self.head = torch.nn.Sequential(
-            torch.nn.Linear(in_out_channels * 32 * 32, 256),
+            torch.nn.Linear(in_channels * size * size, 4096),
             torch.nn.ReLU(),
             torch.nn.Dropout(0.5),
-            torch.nn.Linear(256, 256),
+            torch.nn.Linear(4096, 1024),
+            torch.nn.ReLU(),
+            torch.nn.Dropout(0.5),
+            torch.nn.Linear(1024, 256),
             torch.nn.ReLU(),
             torch.nn.Linear(256, 10)
         )
@@ -51,32 +48,87 @@ class HeadBlock(torch.nn.Module):
         x = torch.nn.Flatten()(x)
         x = self.head(x)
         return x
+    
+class Module(torch.nn.Module):
+    def __init__(self, conv_blocks_number, in_channels = 32, internal_channels = 32, out_channels = 32, bypass = False, max_pool = False):
+        super(Module, self).__init__()
+        self.conv_in = ConvolutionalBlock(
+                    in_channels = in_channels, 
+                    out_channels = internal_channels,
+                    bypass = False
+                )
+        
+        self.conv_blocks_number = conv_blocks_number
+        if(self.conv_blocks_number != 0):
+            self.blocks = torch.nn.Sequential(
+                *[
+                    ConvolutionalBlock(
+                        in_channels = internal_channels, 
+                        out_channels = internal_channels,
+                        bypass = bypass
+                    ) for _ in range(conv_blocks_number)
+                ]
+            )
 
+        self.conv_out = ConvolutionalBlock(
+                    in_channels = internal_channels, 
+                    out_channels = out_channels,
+                    bypass = bypass
+                )
+        
+        self.max_pool = max_pool
+        if(max_pool):
+            self.pool = torch.nn.MaxPool2d(kernel_size=2, stride=2)
+
+    def forward(self, x):
+        x = self.conv_in(x)
+        if(self.conv_blocks_number != 0):
+            x = self.blocks(x) 
+        x = self.conv_out(x)
+        if(self.max_pool):
+            x = self.pool(x)
+        return x
 
 class Network(torch.nn.Module):
-    def __init__(self, blocks_number, in_out_channels = 32, internal_channels = 32):
+    def __init__(self):
         super(Network, self).__init__()
-        self.init_block = InitBlock(in_out_channels = in_out_channels)
-        self.blocks = torch.nn.Sequential(
-            *[
-                ConvolutionalBlock(
-                    in_out_channels = in_out_channels, 
-                    internal_channels = internal_channels
-                ) for _ in range(blocks_number)
-            ]
-        )
-        self.head = HeadBlock(in_out_channels = in_out_channels)
+        self.init_block = InitBlock(out_channels = 16)
+        self.blocks = torch.nn.ModuleList([
+            Module(
+                        conv_blocks_number = 1,
+                        in_channels = 16, 
+                        internal_channels = 32,
+                        out_channels = 32,
+                        bypass = True,
+                        max_pool = False
+                    ),
+            Module(
+                        conv_blocks_number = 1,
+                        in_channels = 32, 
+                        internal_channels = 64,
+                        out_channels = 64,
+                        bypass = True,
+                        max_pool = True
+                    ),
+            Module(
+                        conv_blocks_number = 0,
+                        in_channels = 64, 
+                        internal_channels = 128,
+                        out_channels = 128,
+                        bypass = False,
+                        max_pool = False
+                    )
+        ]) 
+        self.head = HeadBlock(in_channels = 128, size = 16)
 
     def forward(self, x):
         x = self.init_block(x)
         x = torch.nn.functional.relu(x)
-        x = self.blocks(x)
+        for it in self.blocks:
+            x = it(x)
         x = self.head(x)
         return x
     
-
-
-
 def main():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Using device: {device}")
@@ -93,23 +145,19 @@ def main():
 
     train_loader = torch.utils.data.DataLoader(
         train_dataset, 
-        batch_size = 128, 
+        batch_size = 256, 
         shuffle = True, 
         pin_memory = True, 
         num_workers = 4
         )
 
     test_dataset = datasets.ImageFolder(root = "./data/valid", transform = transform)
-    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size = 128, shuffle = False, pin_memory=True, num_workers=4)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size = 512, shuffle = False, pin_memory=True, num_workers=4)
     test_dataset_size = len(test_dataset)
 
 
 
-    model = Network(
-        blocks_number = 2,
-        in_out_channels = 32,
-        internal_channels = 32
-        )
+    model = Network()
 
     model.to(device)
     summary(model, (3, 32, 32))
@@ -118,24 +166,30 @@ def main():
     criterion = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr = 0.001)
 
-    for epoch in range(5):
+    for epoch in range(30):
+        print(f"Using device: {device}")
         start_time = time.time()
         model.train()
+        scaler = GradScaler()
         total_loss = 0
         for images, labels in tqdm.tqdm(train_loader):
             torch.cuda.empty_cache()
             device_images, device_labels = images.to(device), labels.long().to(device)
             optimizer.zero_grad()
-            outputs = model(device_images)
-            loss = criterion(outputs, device_labels)
-            loss.backward()
-            optimizer.step()
+            with torch.amp.autocast(device_type='cuda'):
+                outputs = model(device_images)
+                loss = criterion(outputs, device_labels)
+            
+            scaler.scale(loss).backward()
+            scaler.step(optimizer)
+            scaler.update()
             total_loss += loss.item()
             
         end_time = time.time()
         correctly_predicted = 0
         model.eval()
         with torch.no_grad():
+            print(f"Using device: {device}")
             for images, labels in tqdm.tqdm(test_loader):
                 device_images, device_labels = images.to(device), labels.long().to(device)
                 outputs = model(device_images)
